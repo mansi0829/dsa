30a42c3c9ed4ba928ae926227c8ff1ce8ccb53ca
mansi0829 <mansigundre1@gmail.com>
2022-03-26 11:05:22 +0530

    119

---
+++ leetcode/119PascalsTriangleII.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    vector<int> getRow(int rowIndex)
+    {
+        vector<int> row(rowIndex + 1);
+        row[0] = 1;
+        for (int i = 0; i <= rowIndex; i++)
+        {
+
+            for (int j = i; j > 0; j--)
+            {
+                row[j] = row[j] + row[j - 1];
+            }
+        }
+        return row;
+    }
+};

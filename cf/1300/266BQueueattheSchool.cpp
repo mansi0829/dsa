a7ef507c01f17a7d7243447017725f1e7e5bcf47
mansi0829 <mansigundre1@gmail.com>
2022-04-07 17:05:11 +0530

    266B 1 test case not yet passed

--- cf/1300/266BQueueattheSchool.cpp
+++ cf/1300/266BQueueattheSchool.cpp
@@ -1,13 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int n, t;
-    cin>>n>>t;
-    string arr[n];
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    cin >> n >> t;
+    string st;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> st;
     }
-    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < t; j++)
+        {
+            if (st[i] == 'B' && st[i + 1] == 'G')
+            {
+                swap(st[i], st[i + 1]);
+            }
+        }
+    }
+    cout << st;
+
     return 0;
 }
